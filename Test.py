07fc5c102e9fb51ef8057e27a0ef834e0c84574e
Qinyuan Liu <fengziyuan72@gmail.com>
2022-04-25 13:29:39 +0100

    V1.0.1 Overflow

--- Test.py
+++ Test.py
@@ -3,5 +3,6 @@ import serial
 import time
 ser = serial.Serial('/dev/ttyUSB0')  # open serial port
 print(ser.name)         # check which port was really used
-ser.write(b'hello')     # write a string
+ser.write(b'w')     # write a string
+time.sleep(10)
 ser.close()             # close port
