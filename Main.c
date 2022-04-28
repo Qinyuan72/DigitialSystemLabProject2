6102cc527c266bc5463f272e06adba7c8c21c1d7
Qinyuan Liu <fengziyuan72@gmail.com>
2022-04-28 14:51:28 +0100

    V1.1.1 Comment

--- Main.c
+++ Main.c
@@ -9,20 +9,20 @@
  * This version uses sprintf
  *
  * Created: 25/04/2022 12:25
- *  Author: Mandisi M. Sibanda(20146817), Quinyuan Liu(20137095)
+ * Author: Mandisi M. Sibanda (20146817), Qinyuan Liu (20137095)
  */
 #include <stdio.h> /* Required for sprintf */
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#define DLY_2_ms 61
+#define DLY_12_5_ms 61
 #define COUNT_FOR_10ms 10
+#define ADC_VCF 5 // ADC_Votalge_Covertion_Factor 4.8828125
 
 volatile int8_t new_adc_data_flag; // ISR take action flag
 volatile int8_t new_input_capture_data_flag;
 volatile int8_t new_timer_data_flag;
 
 unsigned int adc_reading; /* Defined as a global here, because it's shared with main  */
-int adc_reading_signed;
 int t_period;
 int t_period_high;
 int t_period_low;
@@ -39,7 +39,6 @@ volatile int8_t ADC_select_flag;
 unsigned char qcntr = 0, sndcntr = 0; /*indexes into the queue*/
 unsigned char queue[60];			  /*character queue*/
 char buffer[60];					  /* similar size to queue */
-unsigned int ADC_VCF;				  // ADC_Votalge_Covertion_Factor 4.8828125
 unsigned int ADC_VR;				  // ADC_Votalge_Reading
 
 void initialisationSection()
@@ -63,7 +62,7 @@ void Init_USART(void)
 	UCSR0A = 0x00; /* Not necessary  */
 
 	UCSR0B = (1 << RXEN0) | (1 << TXEN0) | (1 << TXCIE0); /*enable receiver, transmitter and transmit interrupt, 0x58;*/
-	UBRR0 = 51;										  /*baud rate = 9600, USART 2X = 0 so UBRR0 = ((16*10^6)/(16*19200))-1 = 51.08333333, rounded to 51 */
+	UBRR0 = 51;											  /*baud rate = 19200, USART 2X = 0 so UBRR0 = ((16*10^6)/(16*19200))-1 = 51.08333333, rounded to 51 */
 
 	/* NB: the default state of UCSR0C is 0b00000110; which selects 8 bit, no parity, 1 stop bit */
 	/* Don't be tempted to set it to all zeros - you will select 5 bit data word */
@@ -117,7 +116,7 @@ int main(void)
 					if (qcntr == sndcntr)
 					{
 						sprintf(buffer, "The period of the 555 Timer in microseconds %i", t_period);
-						sendmsg(buffer); /*send first message*/
+						sendmsg(buffer);
 						new_timer_data_flag = 0;
 					}
 				}
@@ -129,189 +128,176 @@ int main(void)
 					if (qcntr == sndcntr)
 					{
 						sprintf(buffer, "This is the ADC value %i", adc_reading);
-						sendmsg(buffer); /*send first message*/
+						sendmsg(buffer);
 					}
 					new_adc_data_flag = 0;
 				}
 			}
 		}
-		/*
-		if (){
-			if the continuous timer value display is selected, report new timer value to the user on the USART
-			Clear the New Input Capture data flag
-		}
-		else Test New ADC data flag to see if new ADC data has been captured.
-			if yes{
-				if continuous ADC display is selected, report new ADC voltage to the user on the USART
-				clear the ADC data flag
-			}
-		*/
 	}
 	return 1;
 }
 
 void chooseChar(char ch, char *buffer)
 {
-	ADC_VCF = 5;
 	switch (ch)
 	{
 	case 'f':
 	case 'F':
 		sprintf(buffer, "Set Direction of motor spin to Forward");
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		PORTB = PORTB & (~(1 << PORTB4));
 		break;
 
 	case 'r':
 	case 'R':
 		sprintf(buffer, "Set Direction of motor spin to Reverse");
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		PORTB = PORTB | ((1 << PORTB4));
 		break;
 
 	case 'b':
 	case 'B':
 		sprintf(buffer, "Turn on Brake");
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		PORTB = PORTB | ((1 << PORTB1));
 		break;
 
 	case 'g':
 	case 'G':
 		sprintf(buffer, "Start Motor");
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		PORTB = PORTB & (~(1 << PORTB1));
 		break;
 
 	case '0':
 		OCR2B = 0;
 		sprintf(buffer, ": Set DC motor speed to %i ", OCR2B);
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		break;
 
 	case '1':
 		OCR2B = 25;
 		sprintf(buffer, ": Set DC motor speed to %i", OCR2B);
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		break;
 
 	case '2':
 		OCR2B = 51;
 		sprintf(buffer, ": Set DC motor speed %i %", OCR2B);
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		break;
 
 	case '3':
 		OCR2B = 77;
 		sprintf(buffer, ": Set DC motor speed %i %", OCR2B);
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		break;
 
 	case '4':
 		sprintf(buffer, ": Set DC motor speed %i %", OCR2B);
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		OCR2B = 102;
 		break;
 
 	case '5':
 		OCR2B = 128;
 		sprintf(buffer, ": Set DC motor speed %i", OCR2B);
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		break;
 
 	case '6':
 		OCR2B = 153;
 		sprintf(buffer, ": Set DC motor speed %i", OCR2B);
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		break;
 
 	case '7':
 		OCR2B = 179;
 		sprintf(buffer, ": Set DC motor speed %i", OCR2B);
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		break;
 
 	case '8':
 		OCR2B = 204;
 		sprintf(buffer, ": Set DC motor speed %i", OCR2B);
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		break;
 
 	case '9':
 		OCR2B = 230;
 		sprintf(buffer, ": Set DC motor speed %i", OCR2B);
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		break;
 	case 's':
 	case 'S':
-		sprintf(buffer, "OCR2B register: %i", OCR2B); // The current value of the OCR2B register is
-		sendmsg(buffer);							  /*send first message*/
+		sprintf(buffer, "OCR2B register: %i", OCR2B);
+		sendmsg(buffer);
 		break;
 
 	case 't':
 	case 'T':
-		sprintf(buffer, "555 Timer period %ius", t_period); // The period of the 555 Timer in microseconds %ius
-		sendmsg(buffer);									/*send first message*/
+		sprintf(buffer, "555 Timer period %ius", t_period);
+		sendmsg(buffer);
 		break;
 
 	case 'l':
 	case 'L':
 		sprintf(buffer, "555 Timer period Low pause %ius", t_period_low);
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		break;
 
 	case 'h':
 	case 'H':
 		sprintf(buffer, "555 Timer period high pause %ius", t_period_high);
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		break;
 
 	case 'c':
 	case 'C':
-		sprintf(buffer, "Timer Report Continuously"); // Continuously
-		sendmsg(buffer);							  /*send first message*/
+		sprintf(buffer, "Timer Report Continuously");
+		sendmsg(buffer);
 		TIMER_report_flag = 1;
 		break;
 
 	case 'e':
 	case 'E':
 		sprintf(buffer, " Stop continuous reporting of Timer input.");
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		TIMER_report_flag = 0;
 		break;
 
 	case 'a':
 	case 'A':
 		sprintf(buffer, "This is the ADC value %i", adc_reading);
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		break;
 
 	case 'v':
 	case 'V':
-		// adc_reading_signed = (int)adc_reading;
-		ADC_VR = adc_reading_signed * ADC_VCF;
-		sprintf(buffer, "Report the ADC conversion result in mV. .%i", ADC_VR);
-		sendmsg(buffer); /*send first message*/
+		ADC_VR = adc_reading * ADC_VCF;
+		sprintf(buffer, "Report the ADC conversion result in %i mV", ADC_VR);
+		sendmsg(buffer);
 		break;
 
 	case 'm':
 	case 'M':
 		sprintf(buffer, "Continuously report the ADC conversion."); // Continuously report the ADC conversion result in mV. You must convert the ADC value to mV.
-		sendmsg(buffer);											/*send first message*/
+		sendmsg(buffer);
 		ADC_report_flag = 1;
 		break;
 
 	case 'n':
 	case 'N':
 		sprintf(buffer, " Stop continuous reporting of ADC input.");
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		ADC_report_flag = 0;
 		break;
 
 	case 'w':
 	case 'W':
 		sprintf(buffer, " move the servomotor to its next position");
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		servo_flag = 1;
 		OCR2A = 15;
 		break;
@@ -319,7 +305,7 @@ void chooseChar(char ch, char *buffer)
 	case 'u':
 	case 'U':
 		sprintf(buffer, "Stop toggling LED bit 4 and stop moving the servomotor");
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		// OCR2A = 30;
 		servo_flag = 0;
 		break;
@@ -327,28 +313,28 @@ void chooseChar(char ch, char *buffer)
 	case 'd':
 	case 'D':
 		sprintf(buffer, " Report to the user the state (in hex) of PORTD outputs.%x", PORTD);
-		sendmsg(buffer); /*send first message*/
-		
+		sendmsg(buffer);
+
 		break;
 
 	case 'p':
 	case 'P':
 		sprintf(buffer, ": Report to the user the state (in hex) of PINB inputs.%x", PINB);
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		break;
 
 	case 'i':
 	case 'I':
 		ADC_select_flag = 1;
 		sprintf(buffer, " Select ADC14, Internal 1.1V BG ref as the ADC input. ");
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		break;
 
 	case 'j':
 	case 'J':
 		ADC_select_flag = 0;
 		sprintf(buffer, ": Select ADC2 as the ADC input. ");
-		sendmsg(buffer); /*send first message*/
+		sendmsg(buffer);
 		break;
 	}
 	new_adc_data_flag = 0;
@@ -365,29 +351,27 @@ ISR(USART_TX_vect)
 ISR(ADC_vect) /* handles ADC interrupts  */
 {
 	adc_reading = ADC; /* ADC is in Free Running Mode - you don't have to set up anything for the next conversion */
-	adc_reading_signed = (int)adc_reading;
-	
+
 	if (ADC_select_flag)
-		ADMUX = ((1 << REFS0) | (0 << ADLAR) |(1<<MUX3)|(1<<MUX2)|(1<<MUX1)|(0<<MUX0));
+		ADMUX = ((1 << REFS0) | (0 << ADLAR) | (1 << MUX3) | (1 << MUX2) | (1 << MUX1) | (0 << MUX0));
 	else
-		ADMUX =  ((1 << REFS0) | (0 << ADLAR) |(0<<MUX3)|(0<<MUX2)|(1<<MUX1)|(0 << MUX0));
-		
-	if (adc_reading > 717)// work out the threshold value needed
-		PORTD = PORTD | (1<<PORTD7);
+		ADMUX = ((1 << REFS0) | (0 << ADLAR) | (0 << MUX3) | (0 << MUX2) | (1 << MUX1) | (0 << MUX0));
+
+	if (adc_reading > 717) // work out the threshold value needed
+		PORTD = PORTD | (1 << PORTD7);
 	else
-		PORTD = PORTD & (~(1<<PORTD7));
-		
+		PORTD = PORTD & (~(1 << PORTD7));
+
 	new_adc_data_flag = 1;
 }
-
 
 ISR(TIMER0_OVF_vect)
 {
 	static uint8_t servo_counter;
 	static uint8_t servo_count_down;
 
-	TCNT0 = DLY_2_ms; /*  TCNT0 needs to be set to the start point each time        */
-	++timecount0;	  /* count the number of times the interrupt has been reached      */
+	TCNT0 = DLY_12_5_ms; /*  TCNT0 needs to be set to the start point each time        */
+	++timecount0;		 /* count the number of times the interrupt has been reached      */
 	if (timecount0 >= COUNT_FOR_10ms)
 	{ /* 10 x 12.48ms = 124.8ms */
 		timecount0 = 0;
@@ -399,6 +383,7 @@ ISR(TIMER0_OVF_vect)
 				servo_direction_flag = 1;
 			if (servo_counter < 12)
 				servo_direction_flag = 0;
+
 			if (servo_direction_flag)
 				OCR2A = servo_counter--;
 			else
